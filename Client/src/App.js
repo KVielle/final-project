c0a14ca1e63a9ae9f9b0f01a76af15112fd1cc68
Keven Vielleuse <Kevielleuse@hotmail.com>
2023-06-29 11:39:27 -0400

    Foundational files in Client and Server

---
+++ Client/src/App.js
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+
+
+
+
+
+
+
+
+function App() {
+  return (
+    <Wrapper>
+
+    </Wrapper>
+  );
+}
+
+
+
+const Wrapper = styled.div``
+
+
+
+
+
+export default App;
